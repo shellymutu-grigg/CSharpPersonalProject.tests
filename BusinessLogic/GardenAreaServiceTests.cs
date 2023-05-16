06475d562c30ad8073d527a9c12e5392ee28acd9
Shelly Mutu-Grigg <shelly.mutu-grigg@serko.com>
2023-05-16 16:18:56 +1200

    UPDATE: Add additional unit tests

--- BusinessLogic/GardenAreaServiceTests.cs
+++ BusinessLogic/GardenAreaServiceTests.cs
@@ -8,12 +8,14 @@ namespace csharp_personal_project.tests.BusinessLogic
         private Mock<MetricSurfaceCalculator> _metricSurfaceCalculator;
 		private Mock<ImperialSurfaceCalculator> _imperialSurfaceCalculator;
 		private GardenAreaService? _underTest;
+		private Mock<BaseGardenArea> _area;
 
 		[SetUp]
         public void Setup()
         {
 			_metricSurfaceCalculator = new Mock<MetricSurfaceCalculator>();
 			_imperialSurfaceCalculator = new Mock<ImperialSurfaceCalculator>();
+			_area = new Mock<BaseGardenArea>();
 		}
 
 		[TestCase(0, 2, ExpectedResult = 0)]
@@ -39,6 +41,47 @@ namespace csharp_personal_project.tests.BusinessLogic
 			return _underTest.GetGardenArea(length, width).SurfaceArea;
 		}
 
+		[TestCase("", ExpectedResult = "Default")]
+		[TestCase(" ", ExpectedResult = "Default")]
+		[TestCase(null, ExpectedResult = "Default")]
+		[TestCase("Lawn", ExpectedResult = "Lawn")]
+		public string GivenGardenAreaService_WhenGetGardenAreaTypeIsCalled_ThenTypeIsReturned(string type)
+		{
+			BaseGardenArea area = new BaseGardenArea
+			{
+				Type = type,
+			};
+			_underTest = new GardenAreaService(_imperialSurfaceCalculator.Object);
+			return _underTest.GetGardenAreaType(area);
+		}
+
+		[TestCase("Lawn")]
+		[TestCase("Vegtable Patch")]
+		public void GivenGardenAreaService_WhenSetGardenAreaTypeIsCalledWithValidData_ThenTypeIsSet(string type)
+		{
+			BaseGardenArea area = new BaseGardenArea
+			{
+				Type = type,
+			};
+			_underTest = new GardenAreaService(_imperialSurfaceCalculator.Object);
+			_underTest.SetGardenAreaType(area, type);
+
+			Assert.That(type, Is.EqualTo(area.Type));
+		}
+
+		[TestCase("")]
+		[TestCase(" ")]
+		[TestCase(null)]
+		public void GivenGardenAreaService_WhenSetGardenAreaTypeIsCalledWithInValidData_ThenInvalidDataExceptionnIsReturned(string type)
+		{
+			BaseGardenArea area = new BaseGardenArea
+			{
+				Type = type,
+			};
+			_underTest = new GardenAreaService(_imperialSurfaceCalculator.Object);
+			var result = Assert.Throws<InvalidDataException>(() => _underTest.SetGardenAreaType(area, type));
+			Assert.That(result.Message, Is.EqualTo("Invalid data"));
+		}
 
 	}
 }
